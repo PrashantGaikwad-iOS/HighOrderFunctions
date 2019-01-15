7010f9717e389957c80c0f66402853c23a362f51
Prashant G <prashant.gaikwad@leftrightmind.com>
2019-01-15 22:03:24 +0530

    Filter, Map, Reduce with example.

--- HighOrderFunctions/ViewController.swift
+++ HighOrderFunctions/ViewController.swift
@@ -10,9 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var myDevices = [Device]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let myiMacPro       = Device(type: "iMac Pro", price: 4999.00, color: "Space Grey")
+        let myiPhone6Plus   = Device(type: "iPhone", price: 799.00, color: "White")
+        let myiPhone7       = Device(type: "iPhone", price: 699.00, color: "Black")
+        let myiPad          = Device(type: "iPad", price: 599.00, color: "Space Grey")
+        let myAppleWatch    = Device(type: "Apple Watch", price: 349.00, color: "Space Grey")
+        let myAppleTV       = Device(type: "Apple TV", price: 199.00, color: "Black")
+        
+        myDevices = [myiMacPro, myiPhone6Plus, myiPhone7, myiPad, myAppleWatch, myAppleTV]
+        
+        
+        
+        // FILTER
+        let iPhones = myDevices.filter({ $0.type == "iPhone"})
+        print(iPhones)
+        //var myPhones: [Device] = []
+        //
+        //for device in myDevices {
+        //    if device.type == "iPhone" {
+        //        myPhones.append(device)
+        //    }
+        //}
+        
+        
+        // MAP
+        let canadianPrices: [Float] = myDevices.map({ $0.price * 1.2})
+        print(canadianPrices)
+        
+        
+        // REDUCE
+        let totalCanadianPrice: Float = canadianPrices.reduce(0.0, +)
+        print(totalCanadianPrice)
+        //var totalPrice: Float = 0.0
+        //
+        //for price in canadianPrices {
+        //    totalPrice += price
+        //}
+        //
+        //print(totalPrice)
+        
     }
 
 
